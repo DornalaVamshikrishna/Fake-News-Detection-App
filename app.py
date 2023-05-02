6864d1c1b9e8c9a5a4d72b5099515e58dad55f36
DornalaVamshikrishna <54587684+DornalaVamshikrishna@users.noreply.github.com>
2023-05-02 12:34:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,52 @@
-#Implement all this concept by machine learning with flask
-
-from flask import Flask, escape, request, render_template
+import streamlit as st
 import pickle
 
 vector = pickle.load(open("vectorizer.pkl", 'rb'))
 model = pickle.load(open("finalized_model.pkl", 'rb'))
-
-app = Flask(__name__)
-
-@app.route('/')
+# Define the content of the home page
 def home():
-    return render_template("index.html")
+    st.write("""
+    # Welcome to the Fake News Detector App!
+    This app uses machine learning to predict the authenticity of news articles. Simply paste in a news article and our model will classify it as reliable or likely to be fake/unreliable. 
+    This tool can be useful for fact-checking and verifying information before sharing it on social media or other platforms.
+    Please use responsibly and always verify information from multiple sources.
+    --- 
+    ### How does it work?
+    Our model was trained on a dataset of labeled news articles to learn the patterns and characteristics of reliable vs. fake/unreliable news. 
+    It takes in the text of a news article as input and outputs a binary classification of 1 (reliable) or 0 (fake/unreliable). 
+    --- 
+    ### About Fake News
+    Fake news refers to deliberate misinformation or hoaxes that are spread through traditional or social media. These false stories can have serious consequences, such as damaging reputations, inciting violence, or influencing political outcomes. 
+    It's important to be aware of the signs of fake news and to fact-check information before sharing it. 
+    ---
+    """)
 
-@app.route('/prediction', methods=['GET', 'POST'])
+
+def predict_news(news):
+    predict = model.predict(vector.transform([news]))[0]
+    return predict
+
 def prediction():
-    if request.method == "POST":
-        news = str(request.form['news'])
-        print(news)
+    st.title("Fake News Prediction App")
 
-        predict = model.predict(vector.transform([news]))[0]
-        print(predict)
+    st.header("Enter the text of the news article below")
+    news = st.text_area("News Text")
 
-        return render_template("prediction.html", prediction_text="News headline is -> {}".format(predict))
+    if st.button("Predict"):
+        if news:
+            prediction = predict_news(news)
+            st.success("News headline is -> {}".format(prediction))
+        else:
+            st.warning("Please enter the text of the news article to make a prediction.")
 
-
-    else:
-        return render_template("prediction.html")
-
+def main():
+    st.sidebar.title("Navigation")
+    menu = ["Home", "Prediction"]
+    choice = st.sidebar.selectbox("Select an option", menu)
+    if choice == "Home":
+        home()
+    elif choice == "Prediction":
+        prediction()
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    main()
